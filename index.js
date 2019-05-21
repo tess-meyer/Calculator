2b2d0499b22f3c4b6ed12db25f8e6e0768d507be
Tess Meyer <tessoliviameyer@gmail.com>
2019-05-21 13:19:33 +1200

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// create a function to insert num to the textvew.
+//  create equal function for = sign to perform once clicked. 
+//  create function to clear the textview when C is selected.
+//  create function to delete one the last selected item
+//   - create a variable equal to the textview.value
+//   - make textview.value equal to new variable and use substring to take one off the end (0, exp.length-1)
+ 
+function insert(num) {
+    document.form.textview.value += num
+}
+
+function equals() {
+
+}
+
+function C() {
+
+}
+
+function back() {
+    
+}
